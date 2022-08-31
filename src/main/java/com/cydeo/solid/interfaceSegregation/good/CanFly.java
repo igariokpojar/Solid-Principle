cb44ce7f678574bf37b32f28e0e0dec76f73617e
Igariok <igariokpojar@yahoo.com>
2022-08-31 09:10:08 -0500

    interface segregation principle good example

---
+++ src/main/java/com/cydeo/solid/interfaceSegregation/good/CanFly.java
@@ -0,0 +1,6 @@
+package com.cydeo.solid.interfaceSegregation.good;
+
+public interface CanFly {
+
+    void fly();
+}
---
+++ src/main/java/com/cydeo/solid/interfaceSegregation/good/CanWalk.java
@@ -0,0 +1,6 @@
+package com.cydeo.solid.interfaceSegregation.good;
+
+public interface CanWalk {
+
+    void walk();
+}
